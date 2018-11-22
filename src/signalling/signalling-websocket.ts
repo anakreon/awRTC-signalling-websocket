7a623357cdfe4eb40a3b34574ac21b44dc493912
Martin Hula <hulamart@gmail.com>
2018-11-22 18:09:31 +0900

    Rewrap signalling data

--- src/signalling/signalling-websocket.ts
+++ src/signalling/signalling-websocket.ts
@@ -1,12 +1,25 @@
 import { SignallingBase } from 'awrtc-signalling';
 
-interface AwSignallingPacket {
+type AwSignallingType = 'register' | 'relay' | 'peerlist';
+type PeerId = string;
+type PeerList = string[];
+
+interface AwRelayData {
     peerId: string;
-    data: AwPeerDataPacket;
+    data: string;
 }
 
-interface AwPeerDataPacket {
-    type: string;
+type AwSignallingData = PeerId | PeerList | AwRelayData;
+
+interface AwSignallingPacket {
+    signallingType: AwSignallingType;
+    signallingData: AwSignallingData;
+}
+
+type AwPeerEventType = 'offer' | 'answer' | 'candidate';
+
+interface AwPeerData {
+    eventType: AwPeerEventType;
     eventData?: RTCSessionDescriptionInit | RTCIceCandidate;
 }
 
@@ -27,38 +40,51 @@ export class WebsocketSignalling extends SignallingBase {
     }
     private onMessage (event: MessageEvent): void {
         console.log('received message: ', event.data);
-        const transferedData = <AwSignallingPacket>JSON.parse(event.data);
-        if (transferedData.data.type === 'offer') {
-            this.handle('offer', { peerId: transferedData.peerId, offer: transferedData.data.eventData });
-        } else if (transferedData.data.type === 'answer') {
-            this.handle('answer', { peerId: transferedData.peerId, answer: transferedData.data.eventData });
-        } else if (transferedData.data.type === 'candidate') {
-            this.handle('newCandidate', { peerId: transferedData.peerId, iceCandidate: transferedData.data.eventData });
+        const signallingPacket = <AwSignallingPacket>JSON.parse(event.data);
+        if (signallingPacket.signallingType === 'peerlist') {
+            this.handle('peerList', { peerList: signallingPacket.signallingData });
+        } else if (signallingPacket.signallingType === 'relay') {
+            const relayData = <AwRelayData>signallingPacket.signallingData;
+            const peerData = <AwPeerData>JSON.parse(relayData.data);
+            if (peerData.eventType === 'offer') {
+                this.handle('offer', { peerId: relayData.peerId, offer: peerData.eventData });
+            } else if (peerData.eventType === 'answer') {
+                this.handle('answer', { peerId: relayData.peerId, answer: peerData.eventData });
+            } else if (peerData.eventType === 'candidate') {
+                this.handle('newCandidate', { peerId: relayData.peerId, iceCandidate: peerData.eventData });
+            }
         }
     }
 
     public registerPeer (peerId: string) {
-        this.sendDataThroughWebsocket('register', peerId);
-    }
-    public sendOfferToRemotePeer (peerId: string, offer: RTCSessionDescriptionInit) {
-        this.sendDataThroughWebsocket('offer', peerId, offer);
-    }
-    public sendAnswerToRemotePeer (peerId: string, answer: RTCSessionDescriptionInit) {
-        this.sendDataThroughWebsocket('answer', peerId, answer);
-    }
-    public sendNewCandidateToRemotePeer (peerId: string, candidate: RTCIceCandidate) {
-        this.sendDataThroughWebsocket('candidate', peerId, candidate);
-    }
-    private sendDataThroughWebsocket (type: string, peerId: string, eventData?: RTCSessionDescriptionInit | RTCIceCandidate): void {
-        const transferData = this.buildWebsocketMessage(type, peerId, eventData);
-        const transferDataJSON = JSON.stringify(transferData);
+        const signallingPacket = {
+            signallingType: 'register',
+            signallingData: peerId
+        };
+        const transferDataJSON = JSON.stringify(signallingPacket);
         this.socket.send(transferDataJSON);
     }
-    private buildWebsocketMessage (type: string, peerId: string, eventData?: RTCSessionDescriptionInit | RTCIceCandidate): AwSignallingPacket {
-        const buildDataPacket = { type, eventData };
-        return { 
-            peerId, 
-            data: buildDataPacket 
+    public sendOfferToRemotePeer (peerId: string, offer: RTCSessionDescriptionInit) {
+        this.relayDataThroughWebsocket('offer', peerId, offer);
+    }
+    public sendAnswerToRemotePeer (peerId: string, answer: RTCSessionDescriptionInit) {
+        this.relayDataThroughWebsocket('answer', peerId, answer);
+    }
+    public sendNewCandidateToRemotePeer (peerId: string, candidate: RTCIceCandidate) {
+        this.relayDataThroughWebsocket('candidate', peerId, candidate);
+    }
+    private relayDataThroughWebsocket (eventType: string, peerId: string, eventData?: RTCSessionDescriptionInit | RTCIceCandidate): void {
+        const signallingPacket = {
+            signallingType: 'relay',
+            signallingData: {
+                peerId: peerId,
+                data: JSON.stringify({
+                    eventType: eventType,
+                    eventData: eventData
+                })
+            }
         };
+        const transferDataJSON = JSON.stringify(signallingPacket);
+        this.socket.send(transferDataJSON);
     }
 }
