488ff4827def6b5c255e77ee5894dc84e363c0dd
Martin Hula <hulamart@gmail.com>
2018-11-26 15:09:48 +0900

    Clean and finish signalling objects

--- src/signalling/signalling-websocket.ts
+++ src/signalling/signalling-websocket.ts
@@ -1,6 +1,6 @@
 import { SignallingBase } from 'awrtc-signalling';
 
-type AwSignallingType = 'register' | 'relay' | 'peerlist';
+type AwSignallingType = 'register' | 'relay' | 'peerlist' | 'close';
 type PeerId = string;
 type PeerList = string[];
 
@@ -10,10 +10,9 @@ interface AwRelayData {
 }
 
 type AwSignallingData = PeerId | PeerList | AwRelayData;
-
 interface AwSignallingPacket {
     signallingType: AwSignallingType;
-    signallingData: AwSignallingData;
+    signallingData: string;
 }
 
 type AwPeerEventType = 'offer' | 'answer' | 'candidate';
@@ -42,9 +41,10 @@ export class WebsocketSignalling extends SignallingBase {
         console.log('received message: ', event.data);
         const signallingPacket = <AwSignallingPacket>JSON.parse(event.data);
         if (signallingPacket.signallingType === 'peerlist') {
-            this.handle('peerList', { peerList: signallingPacket.signallingData });
+            const peerList = <PeerList>JSON.parse(signallingPacket.signallingData);
+            this.handle('peerList', { peerList });
         } else if (signallingPacket.signallingType === 'relay') {
-            const relayData = <AwRelayData>signallingPacket.signallingData;
+            const relayData = <AwRelayData>JSON.parse(signallingPacket.signallingData);
             const peerData = <AwPeerData>JSON.parse(relayData.data);
             if (peerData.eventType === 'offer') {
                 this.handle('offer', { peerId: relayData.peerId, offer: peerData.eventData });
@@ -53,37 +53,43 @@ export class WebsocketSignalling extends SignallingBase {
             } else if (peerData.eventType === 'candidate') {
                 this.handle('newCandidate', { peerId: relayData.peerId, iceCandidate: peerData.eventData });
             }
+        } else if (signallingPacket.signallingType === 'close') {
+
         }
     }
 
-    public registerPeer (peerId: string) {
-        const signallingPacket = {
-            signallingType: 'register',
-            signallingData: peerId
-        };
-        const transferDataJSON = JSON.stringify(signallingPacket);
-        this.socket.send(transferDataJSON);
+    public registerPeer (peerId: PeerId): void {
+        const signallingPacket = this.buildSignallingPacket('register', peerId);
+        this.sendSignallingPacket(signallingPacket);
     }
-    public sendOfferToRemotePeer (peerId: string, offer: RTCSessionDescriptionInit) {
+    public sendOfferToRemotePeer (peerId: PeerId, offer: RTCSessionDescriptionInit) {
         this.relayDataThroughWebsocket('offer', peerId, offer);
     }
-    public sendAnswerToRemotePeer (peerId: string, answer: RTCSessionDescriptionInit) {
+    public sendAnswerToRemotePeer (peerId: PeerId, answer: RTCSessionDescriptionInit) {
         this.relayDataThroughWebsocket('answer', peerId, answer);
     }
-    public sendNewCandidateToRemotePeer (peerId: string, candidate: RTCIceCandidate) {
+    public sendNewCandidateToRemotePeer (peerId: PeerId, candidate: RTCIceCandidate) {
         this.relayDataThroughWebsocket('candidate', peerId, candidate);
     }
-    private relayDataThroughWebsocket (eventType: string, peerId: string, eventData?: RTCSessionDescriptionInit | RTCIceCandidate): void {
-        const signallingPacket = {
-            signallingType: 'relay',
-            signallingData: {
-                peerId: peerId,
-                data: JSON.stringify({
-                    eventType: eventType,
-                    eventData: eventData
-                })
-            }
+    private relayDataThroughWebsocket (eventType: AwPeerEventType, peerId: PeerId, eventData?: RTCSessionDescriptionInit | RTCIceCandidate): void {
+        const peerData = { eventType, eventData };
+        const relayData = this.buildRelayData(peerId, peerData);
+        const signallingPacket = this.buildSignallingPacket('relay', relayData);
+        this.sendSignallingPacket(signallingPacket);
+    }
+    private buildRelayData (peerId: PeerId, peerData: AwPeerData): AwRelayData {
+        return {
+            peerId,
+            data: JSON.stringify(peerData)
+        }
+    }
+    private buildSignallingPacket (signallingType: AwSignallingType, signallingData: AwSignallingData): AwSignallingPacket {
+        return {
+            signallingType,
+            signallingData: JSON.stringify(signallingData)
         };
+    }
+    private sendSignallingPacket (signallingPacket: AwSignallingPacket): void {
         const transferDataJSON = JSON.stringify(signallingPacket);
         this.socket.send(transferDataJSON);
     }
